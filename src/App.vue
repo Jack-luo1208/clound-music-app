e36cd913555d536a06554348515b65a2b27cf2d2
Jack-luo1208 <cctrylyj@163.com>
2022-05-27 11:46:25 +0800

    docs: 完成基本配置

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,53 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="box">
+    <img alt="Vue logo" src="@/assets/images/banner-content1.jpg" class="animation-test" />
+  </div>
 </template>
-<script>
+<script lang="ts">
 export default {
   setup() {
     return {}
   }
 }
 </script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+
+.box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // width: 1080px;
+  height: 420px;
+  background: url('@/assets/images/banner1.jpg');
+  background-position: center center;
+  background-size: 6000px;
+  .animation-test {
+    display: block;
+    width: 730px;
+    transition: ease-in;
+    animation: banner 3.5s infinite;
+  }
+}
+
+@keyframes banner {
+  0% {
+    transition: none 0s ease 0s;
+    opacity: 1;
+  }
+  90% {
+    transition: opacity 1s ease-in 0s;
+    opacity: 0.2;
+  }
+  100% {
+    transition: opacity 1s ease-out 0s;
+    opacity: 1;
+  }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
+const fs = require('fs')
+import { resolve } from 'path'
+// Dotenv 是一个零依赖的模块，它能将环境变量中的变量从 .env 文件加载到 process.env 中
+const dotenv = require('dotenv')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// const envFiles = [
+//   /** default file */
+//   `.env`,
+//   /** mode file */
+//   `.env.${process.env.NODE_ENV}`
+// ]
 
-// https://vitejs.dev/config/
+// for (const file of envFiles) {
+//   const envConfig = dotenv.parse(fs.readFileSync(file))
+//   for (const k in envConfig) {
+//     process.env[k] = envConfig[k]
+//   }
+// }
+
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: './',
+  server: {
+    port: 8080,
+    // 是否自动在浏览器打开
+    open: true,
+    // 是否开启 https
+    https: false,
+    proxy: {
+      '/api': {
+        target: 'https://cloud-music-api-lake.vercel.app/',
+        changeOrigin: true,
+        rewrite: (pathStr) => pathStr.replace('/api', '')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      public: resolve(__dirname, './public')
+    }
+  },
+  optimizeDeps: {
+    include: ['element-plus', 'element-plus/lib/locale/lang/zh-cn', 'dayjs/locale/zh-cn']
+  },
+  build: {
+    // 压缩
+    minify: 'esbuild',
+    assetsDir: ''
+    // outDir: `./dist/${process.env.VITE_ENV}`
+  }
 })
